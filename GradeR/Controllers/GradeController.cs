37cdbeaebf88c114389616404c0ea653ad6170f2
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-04 20:37:51 +0200

    Clean initial project

---
+++ GradeR/Controllers/GradeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GradeR.Controllers
+{
+    public class GradeController : ControllerBase
+    {
+        public IActionResult GetStudentGrades()
+        {
+            return Ok();
+        }
+    }
+}
