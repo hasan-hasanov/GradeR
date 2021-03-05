6e700063294f5f5c7db989f6e8532f62c2eafdf0
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-05 10:37:27 +0200

    Add log information

---
+++ Common/Log/LogEvents.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Log
+{
+    public class LogEvents
+    {
+        public const int ListingItems = 1001;
+        public const int GettingItem = 1002;
+
+        public const int InsertingItem = 1003;
+
+        public const int ListedItems = 2001;
+        public const int GotItem = 2002;
+        public const int InsertedItem = 2003;
+
+        public const int GetItemNotFound = 4000;
+    }
+}
